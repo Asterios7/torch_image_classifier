50e19eb773a21f4d5e762c9e699ca89bd460706f
Asterios7 <asterios812@gmail.com>
2023-09-22 17:02:17 +0200

    Update plot_functions.py

--- utilities/plot_functions.py
+++ utilities/plot_functions.py
@@ -1,5 +1,7 @@
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import torch
+import os
 
 def plot_loss_curves(results: dict) -> None:
     """Plots training curves of a results dictionary using Plotly.
@@ -42,3 +44,53 @@ def plot_loss_curves(results: dict) -> None:
     # Update layout
     fig.update_layout(title_text="Training Curves")
     fig.show()
+
+
+def save_model(model: torch.nn.Module,
+               target_dir: str,
+               model_name: str) -> None:
+    """Saves a PyTorch model to a target directory.
+
+    Args:
+    model: A target PyTorch model to save.
+    target_dir: A directory for saving the model to.
+    model_name: A filename for the saved model. Should include
+      either ".pth" or ".pt" as the file extension.
+
+    Example usage:
+    save_model(model=model_0,
+               target_dir="models",
+               model_name="05_going_modular_tingvgg_model.pth")
+    """
+    # Create target directory if it doesn't exist
+    os.makedirs(target_dir, exist_ok=True)
+
+    # Create model save path
+    assert model_name.endswith(".pth") or model_name.endswith(".pt"), "model_name should end with '.pt' or '.pth'"
+    model_save_path = os.path.join(target_dir, model_name)
+
+    # Save the model state_dict()
+    print(f"[INFO] Saving model to: {model_save_path}")
+    torch.save(obj=model.state_dict(), f=model_save_path)
+
+
+def load_model(model: torch.nn.Module, 
+               model_path: str) -> torch.nn.Module:
+    """Loads a PyTorch model from a specified model path.
+
+    Args:
+    model: An instance of the PyTorch model to load the state_dict into.
+    model_path: The path to the saved model file.
+
+    Example usage:
+    loaded_model = load_model(model=my_model, model_path="models/my_model.pth")
+    """
+    # Load the model state_dict
+    model.load_state_dict(torch.load(model_path))
+
+    # Ensure the model is in evaluation mode
+    model.eval()
+
+    print(f"[INFO] Loaded model from: {model_path}")
+
+    return model
