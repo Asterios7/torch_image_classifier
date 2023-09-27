06ff7dfec70d0ad55f9d71f2a237e913268350fb
Asterios7 <asterios812@gmail.com>
2023-09-27 16:34:29 +0200

    Update predict.py
    - Added option to load model of choice both from mlfflow but also from a models folder where the model.pt files are stored

--- image_classifier/predict.py
+++ image_classifier/predict.py
@@ -1,9 +1,10 @@
 import mlflow.pytorch
 import torch
 import torchvision.transforms as transforms
-from util_functions import load_class_names
 from torchClassifier import torchClassifier
 import argparse
+from util_functions import (load_class_names,
+                            create_effnetb0_model)
 
 # 0. Create an argument parser
 parser = argparse.ArgumentParser(description="Image classification prediction")
@@ -13,32 +14,43 @@ parser.add_argument("--image-path",
                     help="path of the image to classify")
 parser.add_argument("--mlflow-run",
                     type=str,
-                    default='b12a9d24a0ce41a5b219fced426995a5',
+                    default=None,
                     help="MLflow run, retrieves model from this run")
+parser.add_argument("--model-path",
+                    type=str,
+                    default="models/effnetb0_2023-09-27_test-acc-8555.pt",
+                    help="Path of the model to use for predictions")
 
 args = parser.parse_args()
 
 # 1. Set globals
 IMAGE_PATH = args.image_path
 MLFLOW_RUN = args.mlflow_run
+MODEL_PATH = args.model_path
 
 # 2. Set device
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# 3. Load the model from mlflow
-model = mlflow.pytorch.load_model(f"runs:/{MLFLOW_RUN}/model").to(device)
+# 3. Load class_names
+class_names, class_to_idx = load_class_names(
+            class_names_path='./image_classifier/class_names.json',
+            class_to_idx_path='./image_classifier/class_to_idx.json'
+            )
 
-# 4. Create data transform for image
+# 4. Load the model
+if MLFLOW_RUN is not None:
+    model = mlflow.pytorch.load_model(f"runs:/{MLFLOW_RUN}/model").to(device)
+else:
+    model = create_effnetb0_model(num_classes=len(class_names))
+    model.load_state_dict(
+        torch.load(f=MODEL_PATH,
+                   map_location=torch.device(device)))
+
+# 5. Create data transform for image
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor()
 ])
-
-# 5. Load class_names
-class_names, class_to_idx = load_class_names(
-            class_names_path='./train_model/class_names.json',
-            class_to_idx_path='./train_model/class_to_idx.json'
-            )
 
 # 6. Make prediction
 classifier = torchClassifier()
